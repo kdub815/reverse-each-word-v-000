978e77db5980f5495510b93ac46f1e94912f3203
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-12-15 01:45:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,14 @@
 def reverse_each_word(sentence)
+  #convert string into array
   array = sentence.split
-  new_sentence = []
+  #create empty array to put in reversed words
+  reversed_array = []
+  #run each loop to reverse each word in array
   array.each do |word|
     word.reverse!
   end
-  new_sentence <<
+  #put reversed words into new array
+  new_array <<
+  #convert new array back to a string
+  new_array.join
 end
