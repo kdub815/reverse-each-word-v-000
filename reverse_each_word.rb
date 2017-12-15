0f3a84e6071630a46a767ff8d09fb950b01aacc9
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-12-15 01:39:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   array = sentence.split
   new_sentence = []
   array.each do |word|
-      new_sentence << word.reverse_each_word
+      new_sentence << "#{word.reverse_each_word}
   end
 end
